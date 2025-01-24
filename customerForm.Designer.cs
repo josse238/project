a7792afd0ad5b0f9cada16eeae94e8447c1ada04
josse238 <80775230+josse238@users.noreply.github.com>
2025-01-24 15:34:48 +0700

    a

--- customerForm.Designer.cs
+++ customerForm.Designer.cs
@@ -28,18 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.flowLayoutPanelBarang = new System.Windows.Forms.FlowLayoutPanel();
             this.dataGridViewUserItem = new System.Windows.Forms.DataGridView();
+            this.flowLayoutPanelBarang = new System.Windows.Forms.FlowLayoutPanel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUserItem)).BeginInit();
             this.SuspendLayout();
-            // 
-            // flowLayoutPanelBarang
-            // 
-            this.flowLayoutPanelBarang.Location = new System.Drawing.Point(12, 12);
-            this.flowLayoutPanelBarang.Name = "flowLayoutPanelBarang";
-            this.flowLayoutPanelBarang.Size = new System.Drawing.Size(776, 426);
-            this.flowLayoutPanelBarang.TabIndex = 1;
-            this.flowLayoutPanelBarang.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanelBarang_Paint);
             // 
             // dataGridViewUserItem
             // 
@@ -50,6 +42,14 @@
             this.dataGridViewUserItem.RowTemplate.Height = 28;
             this.dataGridViewUserItem.Size = new System.Drawing.Size(773, 423);
             this.dataGridViewUserItem.TabIndex = 0;
+            // 
+            // flowLayoutPanelBarang
+            // 
+            this.flowLayoutPanelBarang.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanelBarang.Name = "flowLayoutPanelBarang";
+            this.flowLayoutPanelBarang.Size = new System.Drawing.Size(776, 426);
+            this.flowLayoutPanelBarang.TabIndex = 1;
+            this.flowLayoutPanelBarang.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanelBarang_Paint);
             // 
             // customerForm
             // 
@@ -68,7 +68,7 @@
 
         #endregion
 
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelBarang;
         private System.Windows.Forms.DataGridView dataGridViewUserItem;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelBarang;
     }
 }
